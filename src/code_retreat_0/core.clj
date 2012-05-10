6f438b950a64631ef3fb4c12d49ae3cd466094a9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-05-10 14:50:44 +0200

    stable.

--- src/code_retreat_0/core.clj
+++ src/code_retreat_0/core.clj
@@ -3,3 +3,34 @@
 
 (fact
   (+ 1 1) => 2)
+
+;; [[0 1 0]
+;;  [0 0 0]
+;;  [0 0 0]]
+
+(defn nb "Compute the state of the neigbours of the cell with coord [y x]"
+  [u y x]
+  (map #(get-in u %)
+       (filter #(not= % [y x])
+        (for [b [-1 0 1]
+                     a [-1 0 1]]
+                 [(+ b y) (+ a x)]))))
+
+(fact
+  (nb [[0 1 0]
+       [0 0 0]
+       [0 0 0]] 1 1) => [0 1 0 0 0 0 0 0])
+
+(defn next-state
+  [u y x]
+  (let [nb-n (reduce + (nb u y x))]
+    (if (< nb-n 2) 0 1)))
+
+;.;. Intellectual 'work' is misnamed; it is a pleasure, a dissipation, and is its own highest reward. -- Twain
+(fact "next time"
+  (next-state [[0 1 0]
+               [0 1 0]
+               [0 0 0]] 1 1) => 0
+  (next-state [[0 1 1]
+               [0 1 0]
+               [0 0 0]] 1 1) => 1)
